600d4cf440f912e358382e7a30ceb283ff46feac
BlueDruddigon <haibuihoang1999@gmail.com>
2023-11-10 21:04:00 +0700

    Feat: Customize the augmentation functions to apply for both data and label

--- datasets/augment.py
+++ datasets/augment.py
@@ -1,46 +1,77 @@
+import random
+from typing import List, Optional, Tuple, Union
+
 import tensorflow as tf
-import tensorflow_datasets as tfds
-from keras import Sequential
-from keras.layers import RandomFlip, RandomRotation
-
-ignore_order = tf.data.Options()
-ignore_order.experimental_deterministic = False
 
 
-def resize_fn(image, img_size):
+class CustomRandomFlip(tf.keras.layers.Layer):
+    modes = ['horizontal_and_vertical', 'horizontal', 'vertical']
+    
+    def __init__(self, mode: str = 'horizontal_and_vertical', seed: Optional[int] = None) -> None:
+        super(CustomRandomFlip, self).__init__()
+        
+        assert mode in self.modes, 'This mode is not supported'
+        
+        self.image_augment = tf.keras.layers.RandomFlip(mode=mode, seed=seed)
+        self.label_augment = tf.keras.layers.RandomFlip(mode=mode, seed=seed)
+    
+    def call(self, image: tf.Tensor, label: tf.Tensor, training: bool = True):
+        if not training:
+            return image, label
+        
+        image = self.image_augment(image)
+        label = self.label_augment(label)
+        
+        return image, label
+
+
+class CustomRandomRotation(tf.keras.layers.Layer):
+    fill_modes = ['constant', 'reflect', 'wrap', 'nearest']
+    interpolations = ['nearest', 'bilinear']
+    
+    def __init__(
+      self,
+      factor: Union[int, List[int], Tuple[int, int]],
+      fill_mode: str = 'reflect',
+      interpolation: str = 'bilinear',
+      seed: Optional[int] = None
+    ) -> None:
+        super(CustomRandomRotation, self).__init__()
+        
+        assert fill_mode in self.fill_modes
+        assert interpolation in self.interpolations
+        
+        self.image_augment = tf.keras.layers.RandomRotation(
+          factor=factor, fill_mode=fill_mode, interpolation=interpolation, seed=seed
+        )
+        self.lable_augment = tf.keras.layers.RandomRotation(
+          factor=factor, fill_mode=fill_mode, interpolation=interpolation, seed=seed
+        )
+    
+    def call(self, image: tf.Tensor, label: tf.Tensor, training: bool = True) -> Tuple[tf.Tensor, tf.Tensor]:
+        if not training:
+            return image, label
+        
+        image = self.image_augment(image)
+        label = self.label_augment(label)
+        
+        return image, label
+
+
+def resize_fn(image, label, img_size):
     image = tf.image.resize_with_crop_or_pad(image, img_size, img_size)
-    return image
+    label = tf.image.resize_with_crop_or_pad(label, img_size, img_size)
+    return image, label
 
 
-augment_fn = Sequential([
-  RandomFlip(),
-  RandomRotation(factor=(-0.2, 0.2)),
-])
-
-
-def make_augmented_ds(img_size, batch_size):
-    (train_ds, valid_ds, test_ds), _ = tfds.load(
-      'synapse',
-      split=['train', 'valid', 'test'],
-      with_info=True,
-      as_supervised=True,
-    )
+def augment_fn(image, label, training: Optional[bool] = None):
+    seed = random.randint(0, 1000)
+    L = tf.keras.Sequential([
+      CustomRandomFlip(seed=seed),
+      CustomRandomRotation(factor=(-0.2, 0.2), seed=seed),
+    ])
     
-    augmented_train_ds = (
-      train_ds.with_options(ignore_order).shuffle(batch_size * 2)
-      .map(lambda x, y: (resize_fn(x, img_size), resize_fn(y, img_size)), num_parallel_calls=tf.data.AUTOTUNE)
-      .map(lambda x, y: (augment_fn(x, training=True), augment_fn(y, training=True)), num_parallel_calls=tf.data.AUTOTUNE)
-      .batch(batch_size)
-      .prefetch(buffer_size=tf.data.AUTOTUNE)
-    )
+    for layer in L.layers:
+        image, label = layer(image, label, training=training)
     
-    augmented_valid_ds = (
-      valid_ds.shuffle(batch_size * 2)
-      .map(lambda x, y: (resize_fn(x, img_size), resize_fn(y, img_size)), num_parallel_calls=tf.data.AUTOTUNE)
-      .batch(batch_size)
-      .prefetch(buffer_size=tf.data.AUTOTUNE)
-    )
-    
-    test_ds = test_ds.batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
-    
-    return augmented_train_ds, augmented_valid_ds, test_ds
+    return image, label
